0ca7ac847e39065b4bb9ece1f3969560a7846cbd
acer <rupasri797@gmail.com>
2020-05-12 20:29:09 +0530

    update new folder course.sh

--- input-parsing/course.sh
+++ input-parsing/course.sh
@@ -8,3 +8,15 @@ if [ ! -f "$1" ]; then
     echo "Given file doesn't exists"
     exit 1
 fi
+
+YAML_FILE=$1
+
+NUMBER_OF_STUDENTS=$(cat $1 |yq . | jq '.STUDENT_LIST | length')
+
+i=0
+while [ $NUMBER_OF_STUDENTS -gt 0 ]; do
+    NAME=$(cat $YAML_FILE | yq . |jq ".STUDENT_LIST[$i].name")
+    echo $NAME
+    i=$(($i+1))
+    NUMBER_OF_STUDENTS=$((NUMBER_OF_STUDENTS-1))
+done
